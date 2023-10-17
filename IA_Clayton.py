0b8a12817e40864f8fd3e3dfbaf2ab9f5afc4b81
Pedro Paulo da Silva Monteiro <pedropaulopyter6@gmail.com>
2023-10-17 15:19:54 -0300

    chrome oppening and ytb command work

--- IA_Clayton.py
+++ IA_Clayton.py
@@ -18,6 +18,14 @@ try:
         voice = listener.listen(source)
         command = listener.recognize_google(voice)
         command = command.lower()
+        chrome_service = Service(r'C:\Program Files\Google\Chrome\Application\chromedriver.exe')
+        chrome_service.command_line_args()
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches',['enable-automation'])
+        options.add_experimental_option('useAutomationExtension',True)
+        options.add_argument('--remote-debugging-port=8989')
+        options.add_argument(r'--user-data-dir=C:\Users\inesg\AppData\Local\Google\Chrome\User_Data')
+
         
         if 'alexa' in command:
             print(command) 
@@ -28,15 +36,30 @@ try:
                 url = url_default+search_query  
 
                 print('abrindo o navegador...') 
-
-                driver = webdriver.Chrome()
+                driver = webdriver.Chrome(service=chrome_service,options=options)
                 driver.get(url)
-                time.sleep(3)
+                time.sleep(2)
             
                 video_get_video = driver.find_element(By.XPATH,xpath_ytb_search)
                 video_get_video.click()
-                print (video_get_video)
-                time.sleep(10) 
+                chromeopen = False
+                while True:
+                    try:
+                        windowsOpen = driver.window_handles
+                        if len(windowsOpen) >= 1:
+                            if not chromeopen: 
+                                print(f"open tabs: {len(windowsOpen)}")
+                                chromeopen = True
+                    except Exception as e:
+                        print(f'Erro: {e}')
+                        print('Chrome closed')
+                        driver.quit() 
+                        break
+                    else:
+                         if len(windowsOpen) < 1: 
+                            print (len(windowsOpen))
+                            
+                  
                
 except sr.WaitTimeoutError:
     print("Tempo limite atingido, pare de ouvir.")
